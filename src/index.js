f99cf5f42061fad316815746a195ca2b4c1be2fb
”AlenaShkr” <elalshkr@gmail.com>
2021-01-27 21:49:45 +0300

    fix naming

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const kindOFCoffe = {
-    
+getData = (promise) => {
+  promise.forEach(el => window.console.log(el));
+}
+
+async function fetchData () {
+    const response = await fetch('src/data.json').then(res => res.json());
+    getData(response);
+}
+window.onload = function load() {
+   const itemKindOfCoffee = document.querySelectorAll('coffee-name');
+   window.console.log(itemKindOfCoffee);
 }
