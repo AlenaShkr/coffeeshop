f66921186ffac2d32a741571bdd957bc3f788833
”AlenaShkr” <elalshkr@gmail.com>
2021-01-27 22:28:48 +0300

    add method get information

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-getData = (promise) => {
-  promise.forEach(el => window.console.log(el));
-}
-
-async function fetchData () {
+async function getData (coffee) {
     const response = await fetch('src/data.json').then(res => res.json());
-    getData(response);
+    const informationAboutCoffe = await response.find(el => el.name === coffee);
+    const copy = Object.assign({}, informationAboutCoffe);
+    return copy;
+    
+}
+async function handlerClick(ev) {
+    let choicedCoffee = ev.target.textContent.toLowerCase();
+    const data = await getData(choicedCoffee);
+    const { name, coffee, milk } = data;
+    window.console.log(name, coffee, milk);
 }
 window.onload = function load() {
-   const itemKindOfCoffee = document.querySelectorAll('coffee-name');
-   window.console.log(itemKindOfCoffee);
+   const listCoffee = document.querySelector('.kind-coffee');
+    listCoffee.addEventListener('click', handlerClick);
+
 }
